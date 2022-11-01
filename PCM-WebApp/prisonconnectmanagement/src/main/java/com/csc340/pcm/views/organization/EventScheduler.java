a603bfc9e30d9e555cbca9e17dc3fabdab7dca0e
Joshua Pellegrino <jepelleg@uncg.edu>
2022-10-31 23:04:21 -0400

    fixed main branch - login functionality for all users

---
+++ PCM-WebApp/prisonconnectmanagement/src/main/java/com/csc340/pcm/views/organization/EventScheduler.java
@@ -0,0 +1,23 @@
+//package com.csc340.pcm.views.organization;
+//
+//import com.csc340.pcm.entity.Contact;
+//import com.csc340.pcm.entity.Event;
+//import com.csc340.pcm.views.MainLayout;
+//import com.vaadin.flow.component.grid.Grid;
+//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.router.PageTitle;
+//import com.vaadin.flow.router.Route;
+//
+//import javax.annotation.security.RolesAllowed;
+//
+//@PageTitle("EventScheduler")
+//@Route(value = "EventScheduler", layout = MainLayout.class)
+//@RolesAllowed("ROLE_ORGANIZATION")
+//public class EventScheduler extends VerticalLayout {
+//
+//    Grid<Event> grid = new Grid<>(Event.class);
+//
+//
+//    public EventScheduler() {
+//    }
+//}
